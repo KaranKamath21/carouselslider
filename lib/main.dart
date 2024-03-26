7e5ae80a3e1e223ab449d4b107457d6b5d07c377
Karan Kamath <202151071@iiitvadodara.ac.in>
2024-03-26 13:31:04 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Carousel Slider Example',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Carousel Slider Example'),
+        ),
+        body: CarouselSliderExample(),
+      ),
+    );
+  }
+}
+
+class CarouselSliderExample extends StatefulWidget {
+  @override
+  _CarouselSliderExampleState createState() => _CarouselSliderExampleState();
+}
+
+class _CarouselSliderExampleState extends State<CarouselSliderExample> {
+  List<String> images = [
+    'assets/1.png',
+    'assets/2.png',
+    'assets/3.png',
+    'assets/4.png',
+    'assets/5.png',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CarouselSlider(
+        options: CarouselOptions(
+          height: 400.0,
+          enlargeCenterPage: true,
+          autoPlay: true,
+          aspectRatio: 16 / 9,
+          autoPlayCurve: Curves.fastOutSlowIn,
+          enableInfiniteScroll: true,
+          autoPlayAnimationDuration: Duration(milliseconds: 800),
+          viewportFraction: 0.8,
+        ),
+        items: images.map((imagePath) {
+          return Builder(
+            builder: (BuildContext context) {
+              return Container(
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.asset(
+                    imagePath,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              );
+            },
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
